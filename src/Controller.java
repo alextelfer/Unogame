4f76492eeb0f6b31212d4651beccdbb8475758e8
FredReinink <fred.rein01@gmail.com>
2017-11-13 15:53:08 -0700

    Add files via upload

--- src/Controller.java
+++ src/Controller.java
@@ -1,16 +1,14 @@
 import java.util.ArrayList;
-
-
 public class Controller {
 	private PlayerHuman player1 = new PlayerHuman();
 	private PlayerAI player2 = new PlayerAI();
 	private PlayerAI player3 = new PlayerAI();
 	private PlayerAI player4 = new PlayerAI();
-	private ArrayList<Card> topCard = new ArrayList<Card>();
+	private ArrayList <Card> topCard = new ArrayList <Card>();
 	
 	private Logic logic = new Logic();
-	private DisplayCards displayCards = new DisplayCards();
 	private Deck deck = new Deck();
+	private DisplayCards display = new DisplayCards();
 	
 	public PlayerHuman getPlayer1() {
 		return player1;
@@ -33,16 +31,20 @@ public class Controller {
 		player2.initialize(deck);
 		player3.initialize(deck);
 		player4.initialize(deck);
-		deck.draw(1,topCard);
+		deck.draw(1, topCard);
 	}
 	
 	public void play() {
-		System.out.println(deck.cardsLeft());
 		boolean gameComplete = false;
-		while (!gameComplete) {
-			if(logic.getPlayerTurn()==1) {
-				displayCards.displayDeck(topCard, deck.getUnoDeck());
-				
+		while (!gameComplete){
+			while (logic.isClockwise()) {
+				if (logic.getPlayerTurn() == 1) {
+					while ()
+					logic.isValid(topCard.get(0),player1.getHand().get(display.displayDeck(topCard,player1.getHand())));		
+				}
+				else if (logic.getPlayerTurn() == 2) {
+					
+				}
 			}
 		}
 	}
