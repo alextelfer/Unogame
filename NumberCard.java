0afca3d6eb247ffa65670c5d810c7c3d7ee05b9b
alextelfer <33209781+alextelfer@users.noreply.github.com>
2017-11-19 16:31:43 -0700

    Add files via upload

---
+++ NumberCard.java
@@ -0,0 +1,34 @@
+
+public class NumberCard extends Card{
+	
+	private String number;
+	public int numval;
+	
+	@Override
+	public String getNumber() {
+		return number;
+	}
+
+	/** Setter for number of type String
+	 * @param number
+	 */
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+	public String toString(){
+		return super.toString() + "" + this.number;		
+	}
+	/** Constructor that creates number cards
+	 * @param col
+	 * @param num
+	 */
+	public NumberCard(int col, int num) {
+		super.colval = col;
+		super.cardColor(col);
+		this.numval = num;
+		this.number = String.valueOf(numval);
+	}
+
+	
+}
---
+++ SpecialFunction.java
@@ -0,0 +1,158 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class SpecialFunction {
+	
+	public int specval;
+	
+	private ArrayList<Card>nextPlayer = new ArrayList<Card>();
+
+	/** Method that implements all the special card functionality
+	 * @param cardPlayed
+	 * @param deck
+	 * @param logic
+	 * @param controller
+	 */
+	public void SpecialFunc(Card cardPlayed, Deck deck, Logic logic, Controller controller) {
+		boolean turn = logic.isClockwise();
+		/** Draw 2 card functionality */
+		if (cardPlayed.getNumber().equals("Draw 2")) {
+			System.out.println(nextPlayer.size());
+			playerCheck(logic,controller);
+			deck.draw(2, nextPlayer);
+			System.out.println(nextPlayer.size());
+			if (turn) {
+				int playerTurn = logic.getPlayerTurn();
+				logic.setPlayerTurn(playerTurn + 2);
+			} else {
+				int playerTurn = logic.getPlayerTurn();
+				logic.setPlayerTurn(playerTurn - 2);
+	
+			}
+				
+			
+		/** Reverse card functionality */
+		} else if (cardPlayed.getNumber().equals("Reverse")) {
+			if (turn) {
+				logic.setPlayerTurn(logic.getPlayerTurn()-1);
+			} else {
+				logic.setPlayerTurn(logic.getPlayerTurn() + 1);
+			}
+			logic.setClockwise(!logic.isClockwise());
+			
+		/** Skip card functionality */
+		} else if (cardPlayed.getNumber().equals("Skip")) {
+			if (turn) {
+				int playerTurn = logic.getPlayerTurn();
+				logic.setPlayerTurn(playerTurn + 2);
+
+			} else {
+				int playerTurn = logic.getPlayerTurn();
+				logic.setPlayerTurn(playerTurn - 2);
+			}
+		/** Wild card functionality */
+		} else if (cardPlayed.getNumber().equals("Wild")) {
+			if (logic.getPlayerTurn() == 1 || logic.getPlayerTurn() == 2) {
+				WildCard(controller);
+			}
+			if (turn) {
+				int playerTurn = logic.getPlayerTurn();
+				logic.setPlayerTurn(playerTurn + 1);
+
+			} else {
+				int playerTurn = logic.getPlayerTurn();
+				logic.setPlayerTurn(playerTurn - 1);
+			}
+		/** Wild Draw 4 functionality */
+		} else if (cardPlayed.getNumber().equals("Wild Draw 4")) {
+			if (logic.getPlayerTurn() == 1 || logic.getPlayerTurn() == 2) {
+				WildCard(controller);
+			}
+			playerCheck(logic, controller);
+			deck.draw(4, nextPlayer);
+			if (turn) {
+				int playerTurn = logic.getPlayerTurn();
+				logic.setPlayerTurn(playerTurn + 2);
+			} else {
+				int playerTurn = logic.getPlayerTurn();
+				logic.setPlayerTurn(playerTurn - 2);
+			}
+		}
+		else {
+			if (turn) {
+				logic.setPlayerTurn(logic.getPlayerTurn()+1);
+			}
+			else {
+				logic.setPlayerTurn(logic.getPlayerTurn()-1);
+			}
+		}
+	}
+	
+	/**Method for Wild cards that takes input to choose Top card color
+	 * @param controller
+	 */
+	public void WildCard(Controller controller) {
+		System.out.println("Choose a color: [1] RED, [2] BLUE, [3] YELLOW, [4] GREEN");
+		Scanner keyboard = new Scanner(System.in);
+		int next = keyboard.nextInt();
+		if (next == 1) {
+			SpecialCard scard = new SpecialCard(0, -1);
+			controller.setCardPlayed(scard);
+		} else if (next == 2 ) {		
+			SpecialCard scard = new SpecialCard(1, -1);
+			controller.setCardPlayed(scard);
+		} else if (next == 3) {		
+			SpecialCard scard = new SpecialCard(2, -1);
+			controller.setCardPlayed(scard);
+		} else if (next == 4) {		
+			SpecialCard scard = new SpecialCard(3, -1);
+			controller.setCardPlayed(scard);
+		}
+	}
+	
+	/** Method that checks who the next player is for Draw 2 cards and Wild Draw 4 cards.
+	 * @param logic
+	 * @param controller
+	 * @return ArrayList<Card> of the nextPlayer
+	 */
+	public ArrayList<Card> playerCheck(Logic logic, Controller controller) {
+		/**PRIVACY LEAK: EDITING THROUGH GETTER; NEEDED BECAUSE CHANGSE TO NEXTPLAYER NEED TO HAPPEN TO 
+		 *THE PLAYER HAND LISTS
+		REPLACE RETURN WITH SET() */
+		boolean turn = logic.isClockwise();
+		if (turn) {
+			int thisPlayer = logic.getPlayerTurn();
+			if (thisPlayer == 1) {
+				nextPlayer = controller.getPlayer2().getHand();
+				return nextPlayer;
+			} else if (thisPlayer == 2) {
+				nextPlayer = controller.getPlayer3().getHand();
+				return nextPlayer;
+			} else if (thisPlayer == 3) {
+				nextPlayer = controller.getPlayer4().getHand();
+				return nextPlayer;
+			} else if (thisPlayer == 4) {
+				nextPlayer = controller.getPlayer1().getHand();
+				return nextPlayer;
+			}
+		} else {
+			int thisPlayer = logic.getPlayerTurn();
+			if (thisPlayer == 1) {
+				nextPlayer = controller.getPlayer4().getHand();
+				return nextPlayer;
+			} else if (thisPlayer == 2) {
+				nextPlayer = controller.getPlayer1().getHand();
+				return nextPlayer;
+			} else if (thisPlayer == 3) {
+				nextPlayer = controller.getPlayer2().getHand();
+				return nextPlayer;
+			} else if (thisPlayer == 4) {
+				nextPlayer = controller.getPlayer3().getHand();
+				return nextPlayer;
+			}
+		}
+		return nextPlayer;
+	}
+	
+	
+}
