1d4de16bad1f9889574ebd4f60aab53edea82a37
FredReinink <fred.rein01@gmail.com>
2017-11-20 15:47:42 -0700

    Delete Uno.java

--- Runner/Uno.java
+++
@@ -1,9 +0,0 @@
-package Runner;
-
-public class Uno {
-	public static void main(String [] args) {
-		Controller controller = new Controller();
-		controller.initializeCards();
-		controller.play();
-	}
-}
