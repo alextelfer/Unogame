7b7b7f3c1fbf8deca68dcd23ca28e553b5ff816b
Trilok Patel <33501271+patelTrilok@users.noreply.github.com>
2017-11-19 19:27:32 -0700

    Add files via upload

---
+++ Logic/Logic.java
@@ -0,0 +1,64 @@
+package Logic;
+import Card.Card;
+import Player.PlayerAI;
+import Player.PlayerHuman;
+
+public class Logic {
+	private int playerTurn = 1;
+	private boolean clockwise = true;
+	
+	public int getPlayerTurn() {
+		return playerTurn;
+	}
+
+	public void setPlayerTurn(int playerTurn) {
+		this.playerTurn = playerTurn;
+	}
+
+	public boolean isClockwise() {
+		return clockwise;
+	}
+
+	public void setClockwise(boolean clockwise) {
+		this.clockwise = clockwise;
+	}
+	
+	public boolean isValid(Card topCard, Card cardPlayed){
+		boolean valid = false;
+		String topCardColor = topCard.getColor();
+		String topCardNumber = topCard.getNumber();
+		String cardPlayedColor = cardPlayed.getColor();
+		String cardPlayedNumber = cardPlayed.getNumber();
+		System.out.println("Colors:  " + cardPlayedColor + "  " + topCardColor);
+		System.out.println("Numbers:  " + cardPlayedNumber + "  " + topCardNumber);
+		if (cardPlayedColor.equals(topCardColor) || cardPlayedNumber.equals(topCardNumber) || cardPlayedNumber.equals("Wild") || cardPlayedNumber.equals("Wild Draw 4")) {
+			valid = true;
+		}
+		System.out.println(valid);
+		return valid;
+	}
+	
+	public void gameState(){
+		if (playerTurn == 5) {
+			setPlayerTurn(1);
+		} 
+		else if (playerTurn == 6) {
+			setPlayerTurn(2);
+		}
+		if (playerTurn ==0 ) {
+			setPlayerTurn(4);
+		} else if (playerTurn == -1) {
+			setPlayerTurn(3);
+		}
+	}
+	public void numOfCards(PlayerHuman player1, PlayerAI player2, PlayerAI player3, PlayerAI player4) {
+		int numPlayer1 = player1.getHand().size();
+		int numPlayer2 = player2.getHand().size();
+		int numPlayer3 = player3.getHand().size();
+		int numPlayer4 = player4.getHand().size();
+		System.out.println("Player 1 has " + numPlayer1 + " Cards");
+		System.out.println("Player 2 has " + numPlayer2 + " Cards");
+		System.out.println("Player 3 has " + numPlayer3 + " Cards");
+		System.out.println("Player 4 has " + numPlayer4 + " Cards");
+	}
+}
---
+++ Runner/RunUno.java
@@ -0,0 +1,19 @@
+package Runner;
+
+import Runner.Controller;
+import GUI.UnoActionPerformer;
+import GUI.UnoGUI;
+
+public class RunUno {
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				Controller controller = new Controller();
+				controller.initializeCards();
+				UnoActionPerformer actions = new UnoActionPerformer(controller);
+				UnoGUI gui = new UnoGUI();
+				gui.display(controller,actions);
+			}
+		});
+	}
+}
