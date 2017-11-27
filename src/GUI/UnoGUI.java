5e4608ff771d253001585725a20271a5e4049f87
alextelfer <33209781+alextelfer@users.noreply.github.com>
2017-11-26 19:25:21 -0700

    Update UnoGUI.java

--- src/GUI/UnoGUI.java
+++ src/GUI/UnoGUI.java
@@ -25,7 +25,12 @@ public class UnoGUI extends JFrame {
 	private JButton card = new JButton();
 	private Card aCard;
 
+	private JFrame gameWindow = new JFrame();
+	private JFrame wildCardWindow = new JFrame();
 	private JPanel playerPanel = new JPanel();
+	private JPanel wildPanel = new JPanel();
+
+	Color blueCard = new Color(66, 104, 208);
 
 	/**
 	 * Creates display window and displays the topCard. Calls cardButton to display
@@ -38,27 +43,29 @@ public class UnoGUI extends JFrame {
 
 	public void display(Controller game, ActionListener listener) {
 
-		this.getContentPane().removeAll();
-		this.getContentPane().revalidate();
-		this.getContentPane().repaint();
+		gameWindow.getContentPane().removeAll();
+		gameWindow.getContentPane().revalidate();
+		gameWindow.getContentPane().repaint();
 
-		this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		gameWindow.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		gameWindow.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 		JLabel player2Cards = new JLabel("Player 2 has: " + game.getPlayer2().getHand().size() + " Cards");
 		JLabel player3Cards = new JLabel("Player 3 has: " + game.getPlayer3().getHand().size() + " Cards",
 				JLabel.CENTER);
 		JLabel player4Cards = new JLabel("Player 4 has: " + game.getPlayer4().getHand().size() + " Cards");
-		this.getContentPane().add(player2Cards, BorderLayout.WEST);
-		this.getContentPane().add(player3Cards, BorderLayout.NORTH);
-		this.getContentPane().add(player4Cards, BorderLayout.EAST);
+		gameWindow.getContentPane().add(player2Cards, BorderLayout.WEST);
+		gameWindow.getContentPane().add(player3Cards, BorderLayout.NORTH);
+		gameWindow.getContentPane().add(player4Cards, BorderLayout.EAST);
 
 		String topCardAsString = game.getTopCard().get(0).toString();
 		JLabel topCardLabel = new JLabel("Top Card: " + topCardAsString, JLabel.CENTER);
 
-		this.getContentPane().add(topCardLabel, BorderLayout.CENTER);
+		gameWindow.getContentPane().add(topCardLabel, BorderLayout.CENTER);
 		cardButton(game.getPlayer1().getHand(), listener);
-		this.setVisible(true);
+		wildCardButtons(listener);
+		gameWindow.setVisible(true);
+
 	}
 
 	/**
@@ -69,23 +76,55 @@ public class UnoGUI extends JFrame {
 	 *            ActionListener
 	 */
 	public void cardButton(ArrayList<Card> playerCards, ActionListener listener) {
-		System.out.println("The number of cards..........." + playerCards.size());
+
 		playerPanel = new JPanel();
 		for (int i = 0; i < playerCards.size(); i++) {
 			aCard = playerCards.get(i);
 			String aFace = aCard.toString();
 			card = new JButton(aFace);
+			if (aCard.getColor() == "Red ") {
+				card.setBackground(Color.RED);
+			} else if (aCard.getColor() == "Blue ") {
+				card.setBackground(blueCard);
+			} else if (aCard.getColor() == "Yellow ") {
+				card.setBackground(Color.YELLOW);
+			} else if (aCard.getColor() == "Green ") {
+				card.setBackground(Color.GREEN);
+			} else {
+				card.setBackground(Color.LIGHT_GRAY);
+			}
+
 			String stringI = Integer.toString(i + 1);
 			card.setActionCommand(stringI);
 			card.addActionListener(listener);
 			playerPanel.add(card);
 		}
 		JButton drawCardButton = new JButton("Draw Card");
-		drawCardButton.setBackground(Color.CYAN);
+		drawCardButton.setBackground(Color.MAGENTA);
 		drawCardButton.setActionCommand(Integer.toString(0));
 		drawCardButton.addActionListener(listener);
 		playerPanel.add(drawCardButton);
-		this.getContentPane().add(playerPanel, BorderLayout.SOUTH);
+		gameWindow.getContentPane().add(playerPanel, BorderLayout.SOUTH);
+	}
+
+	public void wildCardButtons(ActionListener listener) {
+		JButton redColor = new JButton("RED");
+		redColor.setActionCommand("RED");
+		redColor.addActionListener(listener);
+		wildPanel.add(redColor);
+		JButton blueColor = new JButton("BLUE");
+		blueColor.setActionCommand("BLUE");
+		blueColor.addActionListener(listener);
+		wildPanel.add(blueColor);
+		JButton yellowColor = new JButton("YELLOW");
+		yellowColor.setActionCommand("YELLOW");
+		yellowColor.addActionListener(listener);
+		wildPanel.add(yellowColor);
+		JButton greenColor = new JButton("GREEN");
+		greenColor.setActionCommand("GREEN");
+		greenColor.addActionListener(listener);
+		wildPanel.add(greenColor);
+		wildCardWindow.getContentPane().add(wildPanel, BorderLayout.CENTER);
 	}
 
 }
